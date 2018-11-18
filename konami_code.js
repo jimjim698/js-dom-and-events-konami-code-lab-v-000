83099e4d4c6502449c7aac4e59ac1b159ed6dbb9
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-18 00:59:45 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -9,7 +9,7 @@ function init() {
   
   
   function onKeyDownHandler(e){
-      const key = parseInt(e.detail || e.location);
+      const key = parseInt(e.detail || e.which);
     
     if( key === code[index]){
       index++;
